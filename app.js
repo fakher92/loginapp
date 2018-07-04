7a2699ce2e831fb874e9b226088a618ed7a16c21
Fakher Alhamad <fakher1281992@gmail.com>
2018-07-04 10:19:18 +0200

    Add middleware for the validator

--- app.js
+++ app.js
@@ -41,3 +41,23 @@ app.use(
     resave: true
   })
 );
+
+// Express Validator
+app.use(
+  expressValidator({
+    errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.'),
+        root = namespace.shift(),
+        formParam = root;
+
+      while (namespace.length) {
+        formParam += '[' + namespace.shift() + ']';
+      }
+      return {
+        param: formParam,
+        msg: msg,
+        value: value
+      };
+    }
+  })
+);
