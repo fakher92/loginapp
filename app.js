dfcfec746f67524ccc9f981efbfb016517882106
Fakher Alhamad <fakher1281992@gmail.com>
2018-07-12 22:52:27 +0200

    create user schema & test register user

--- app.js
+++ app.js
@@ -82,8 +82,8 @@ app.use('/', routes);
 app.use('/users', users);
 
 // Set Port
-app.set('port', process.env.PORT || 3000);
+var port = process.env.PORT || 3000;
 
-app.listen(app.get('port'), function() {
-  console.log('Server started on port ' + app.get('port'));
+app.listen(port, function() {
+  console.log('Server started on port ' + port);
 });
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-// // Register
+// Register
 router.get('/register', function(req, res) {
   res.render('register');
 });
@@ -9,6 +9,16 @@ router.get('/register', function(req, res) {
 // Login
 router.get('/login', function(req, res) {
   res.render('login');
+});
+
+// Register User
+router.post('/register', function(req, res) {
+  var name = req.body.name;
+  var email = req.body.email;
+  var username = req.body.username;
+  var password = req.body.password;
+  var password2 = req.body.password2;
+  console.log(name);
 });
 
 module.exports = router;
