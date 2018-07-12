41bf10c70bb1062b3a63c9bca6d4f8fcd0e15f99
Fakher Alhamad <fakher1281992@gmail.com>
2018-07-12 11:29:50 +0200

    add submit away for route files & set the port

--- app.js
+++ app.js
@@ -73,3 +73,13 @@ app.use(function(req, res, next) {
   res.locals.user = req.user || null;
   next();
 });
+
+app.use('/', routes);
+app.use('/users', users);
+
+// Set Port
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function() {
+  console.log('Server started on port ' + app.get('port'));
+});
