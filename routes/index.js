7e8f09033b88eae6b7725dc7ed4abffc0e1c2958
Fakher Alhamad <fakher1281992@gmail.com>
2018-07-13 13:09:02 +0200

    fix a bug

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,13 @@ router.get('/', function(req, res) {
   res.render('index');
 });
 
+ensureAuthenticated = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return next();
+  } else {
+    //req.flash('error_msg','You are not logged in');
+    res.redirect('/users/login');
+  }
+};
+
 module.exports = router;
