fcbfb7c1b154917f8b6b3c8fe9baf12903aa7ef5
Fakher Alhamad <fakher1281992@gmail.com>
2018-08-17 13:44:13 +0200

    fixed the database code

--- app.js
+++ app.js
@@ -14,9 +14,80 @@ var mongo = require('mongodb');
 var mongoose = require('mongoose');
 require('dotenv').config({ path: 'variables.env' });
 
-mongoose.connect('mongodb://localhost/loginapp');
-var db = mongoose.connection;
+// mongoose.connect('mongodb://localhost/loginapp');
+// var db = mongoose.connection;
+//==================================================//
 
+// connect to our database and handle any bad connections
+let mongooseOptions = {};
+
+if (
+  process.env.DATABASE_USERNAME &&
+  process.env.DATABASE_PASSWORD &&
+  process.env.DATABASE_USERNAME.trim() !== '' &&
+  process.env.DATABASE_PASSWORD.trim() !== ''
+) {
+  mongooseOptions = {
+    auth: {
+      user: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD
+    }
+  };
+}
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(
+    `mongodb://${process.env.DATABASE_HOST || localhost}:${process.env
+      .DATABASE_PORT || 27017}/${process.env.DATABASE_NAME ||
+      'express-mongo-boilerplate'}`,
+    mongooseOptions
+  )
+  .catch(error => {
+    console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${error.message}`);
+    process.exit(1);
+  });
+
+// load all models
+require('./models/ToDo');
+
+// load the app
+const app = require('./app');
+
+// define SSL/TLS options
+let tlsEnabled = false;
+let tlsOptions = {};
+
+if (
+  process.env.SSL === 'on' &&
+  process.env.SSL_CERT != undefined &&
+  process.env.SSL_KEY != undefined &&
+  process.env.SSL_CERT != '' &&
+  process.env.SSL_KEY != ''
+) {
+  tlsEnabled = true;
+
+  try {
+    tlsOptions = {
+      key: fs.readFileSync(process.env.SSL_KEY),
+      cert: fs.readFileSync(process.env.SSL_CERT)
+    };
+
+    if (process.env.SSL_CHAIN != undefined && process.env.SSL_CHAIN != '') {
+      tlsOptions.ca = fs.readFileSync(process.env.SSL_CHAIN);
+    }
+
+    if (process.env.SSL_DHPARAM != undefined && process.env.SSL_DHPARAM != '') {
+      tlsOptions.dhparam = fs.readFileSync(process.env.SSL_DHPARAM);
+    }
+  } catch (e) {
+    console.error(`\n!!! ${e.message}\n`);
+    console.error('=> SSL could not be enabled. Using fallback.\n');
+    tlsEnabled = false;
+  }
+}
+
+//===================================================//
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
@@ -42,7 +113,8 @@ app.use(
     secret: process.env.SESSION_SECRET || 'notaverysecuresecret',
     key: process.env.SESSION_KEY || 'notaverysecurekey',
     resave: false,
-    saveUninitialized: false
+    saveUninitialized: false,
+    store: new MongoStore({ mongooseConnection: mongoose.connection })
   })
 );
 
